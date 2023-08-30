1c1090431c14513fdac53f35d2623b992f34110d
aaronbee2010 <55222762+aaronbee2010@users.noreply.github.com>
2023-08-30 03:12:09 +0100

    Add failsafe to .Add_v2_suffix.sh to prevent execution by non-developer

--- .Add_v2_suffix.sh
+++ .Add_v2_suffix.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+#
+# Add failsafe to prevent a non-developer from running this script
+if [[ $# -ne 1 ]] ; then
+  printf "Usage: source %s dummy\n" "$0"
+  printf "  dummy is anything; required to avoid direct user click.\n"
+  printf "  This should only be called from the WGSE internal scripts.\n"
+  (return 0 2>/dev/null) && return || exit
+fi
 
 for script in {.,./scripts}/*.sh; do
     sed -i 's/\.sh/_v2.sh/g' ${script}
